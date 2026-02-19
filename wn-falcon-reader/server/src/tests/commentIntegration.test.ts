9146d1f4c994246af5a956fab39c1ec7a9fa5d69
Guevaer Arnaud <guevaer8@gmail.com>
2026-02-19 15:48:26 +0100

    test(reader): refactor comment integration tests

--- wn-falcon-reader/server/src/tests/commentIntegration.test.ts
+++ wn-falcon-reader/server/src/tests/commentIntegration.test.ts
@@ -5,27 +5,54 @@ import { AppDataSource } from "../config/data-source.js";
 import { pool } from "./setup.js";
 
 describe("Comments Integration", () => {
+  let articleId: number;
+
+  // We wait for the DataSource to initialize before running any tests
   beforeAll(async () => {
     if (!AppDataSource.isInitialized) {
       await AppDataSource.initialize();
     }
   });
 
+  // We reset the database before each test
   beforeEach(async () => {
     await pool.query("TRUNCATE writer.t_articles RESTART IDENTITY CASCADE");
+
+    const newArticle = await pool.query(
+      `INSERT INTO writer.t_articles (title, subtitle, summary, content, id_category) 
+       VALUES ($1, $2, $3, $4, $5) 
+       RETURNING id_article`,
+      ["Article de Test", "Sous-titre", "Résumé", "Le corps de l'article", 1],
+    );
+
+    articleId = newArticle.rows[0].id_article;
+
+    await pool.query(
+      `INSERT INTO reader.t_comments (description, article_id) 
+       VALUES ($1, $2)`,
+      ["Premier commentaire", articleId],
+    );
+
+    await pool.query(
+      `INSERT INTO reader.t_comments (description, article_id) 
+       VALUES ($1, $2)`,
+      ["Deuxième commentaire", articleId],
+    );
+  });
+
+  // ----- Get should success -----
+  it("should return 200 and a list of comments for an article", async () => {
+    const response = await request(app).get(`/articles/${articleId}/comments`);
+
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBe(2);
+    expect(response.body[0].description).toBe("Deuxième commentaire"); //comments are ordered by last created first
+    expect(response.body[1].description).toBe("Premier commentaire");
   });
 
   // ----- Post should success -----
   it("should return 201 and create a new comment", async () => {
-    const newArticle = await pool.query(
-      `INSERT INTO writer.t_articles (title, subtitle, summary, content, id_category) 
-     VALUES ($1, $2, $3, $4, $5)
-     RETURNING id_article`,
-      ["Un titre", "Un sous-titre", "Un résumé", "Le corps de l'article", 1],
-    );
-
-    const articleId = newArticle.rows[0].id_article;
-
     const response = await request(app)
       .post(`/articles/${articleId}/comments`)
       .send({ description: "Ceci est un commentaire de test :)" });
@@ -38,32 +65,14 @@ describe("Comments Integration", () => {
     expect(response.body.id).toBeDefined();
   });
 
-  //   // ----- Get should Success -----
-  // it("should get comments for a specific article", async () => {
-  //   vi_actions.getMany.mockResolvedValue([
-  //     { id: 1, description: "Commentaire test", article_id: 1 },
-  //   ]);
+  // ----- Post should fail -----
+  it("should return 400 if description is missing", async () => {
+    const response = await request(app)
+      .post(`/articles/${articleId}/comments`)
+      .send({});
 
-  //   const response = await request(app).get("/articles/1/comments");
-
-  //   expect(response.status).toBe(200);
-  //   expect(Array.isArray(response.body)).toBe(true);
-  //   expect(response.body[0].description).toBe("Commentaire test");
-  // });
-  //   // ----- Get should fail -----
-  //   it("should return 500 if there's an error fetching comments", async () => {
-  //     vi_actions.getMany.mockRejectedValue(new Error("DB error"));
-  //     const response = await request(app).get("/articles/1/comments");
-
-  //     expect(response.status).toBe(500);
-  //     expect(response.body.error).toBe("Internal server error.");
-  //   });
-
-  //   // ----- Post should fail -----
-  //   it("should return 400 if description is missing", async () => {
-  //     const response = await request(app).post("/articles/1/comments").send({});
-
-  //     expect(response.status).toBe(400);
-  //     expect(response.body.error).toBe("Le message est requis.");
-  //   });
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe("Le message est requis.");
+    expect(response.body).not.toHaveProperty("body");
+  });
 });
